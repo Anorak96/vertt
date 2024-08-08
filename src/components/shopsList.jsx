88302ffa7004533f1711fd0e2e454187e520b72e
David Duyile <femiduyile@gmail.com>
2024-08-08 23:27:29 +0100

    Update shopsList.jsx

--- src/components/shopsList.jsx
+++ src/components/shopsList.jsx
@@ -5,7 +5,7 @@ const shopsList = ({shop}) => {
     return (
         <>
             <Card className='productlist-card'>
-                <Card.Img className='home-product-img' src={`http://127.0.0.1:8000${shop.image}`} variant='top' alt={shop.name} />
+                <Card.Img className='home-product-img' src={`${shop.image}`} variant='top' alt={shop.name} />
                 <Card.Body style={{'padding': '5px'}}>
                     <Card.Title>{shop?.name}</Card.Title>
                 </Card.Body>
